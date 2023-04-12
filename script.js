13852dcda1774b4586620678615097b71953103b
chrisres91 <130558864+chrisres91@users.noreply.github.com>
2023-04-12 21:25:01 +1000

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ var clownBtn = document.getElementById('clownBtn');
 var clownImg = document.getElementById('clownImg');
 
 clownBtn.addEventListener('mousedown', function() {
-  // Generate random delay between 0 and 5 seconds (5000 milliseconds)
-  var delay = Math.floor(Math.random() * 5000);
+  // Generate random delay between 2.5 and 5 seconds (2500 and 5000 milliseconds)
+  var delay = Math.floor(Math.random() * 2500) + 2500;
 
   // Show clown image after the random delay
   setTimeout(function() {
