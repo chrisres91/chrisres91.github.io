73cf460d841a234de2cb2b34969bf15da4997437
chrisres91 <130558864+chrisres91@users.noreply.github.com>
2023-04-13 18:06:12 +1000

    trying new

--- script.js
+++ script.js
@@ -1,17 +1,48 @@
 var clownBtn = document.getElementById('clownBtn');
 var clownImg = document.getElementById('clownImg');
+var bar = document.getElementById('bar');
+var barContainer = document.querySelector('.bar-container');
+var holding = false;
+var barInterval;
 
 clownBtn.addEventListener('mousedown', function() {
-  // Generate random delay between 2.5 and 5 seconds (2500 and 5000 milliseconds)
-  var delay = Math.floor(Math.random() * 2500) + 2500;
+    // Reset the bar
+    bar.style.width = '0%';
+    // Start the bar fill animation
+    barInterval = setInterval(function() {
+        bar.style.width = parseFloat(bar.style.width) + 1 + '%';
+    }, 50);
+    holding = true;
+    // Generate random delay between 2.5 and 5 seconds (2500 and 5000 milliseconds)
+    var delay = Math.floor(Math.random() * 2500) + 2500;
+    // Show clown image after the random delay
+    setTimeout(function() {
+        // Stop the bar fill animation
+        clearInterval(barInterval);
+        // Hide the bar
+        barContainer.style.display = 'none';
+        clownImg.style.display = 'block';
+    }, delay);
+});
 
-  // Show clown image after the random delay
-  setTimeout(function() {
-    clownImg.style.display = 'block';
-  }, delay);
+clownBtn.addEventListener('mouseup', function() {
+    if (holding) {
+        // Reset holding status and hide the clown image
+        holding = false;
+        clownImg.style.display = 'none';
+        // Reset the bar
+        bar.style.width = '0%';
+        barContainer.style.display = 'block';
+    }
+});
 
-  // Hide clown image after 3 seconds (3000 milliseconds)
-  setTimeout(function() {
-    clownImg.style.display = 'none';
-  }, delay + 3000);
+clownBtn.addEventListener('mouseout', function() {
+    if (holding) {
+        // Reset holding status and hide the clown image
+        holding = false;
+        clownImg.style.display = 'none';
+        // Reset the bar
+        bar.style.width = '0%';
+        barContainer.style.display = 'block';
+    }
 });
